a5e70a9eb1fa0a23146c27fc69abfd1cab536f9d
momakas <kkgtycb@yahoo.co.jp>
2025-06-28 16:39:05 +0900

    第十章部分プレレンダリングの有効化

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import SideNav from "../ui/dashboard/sidenav";
 
-export default function Layout({ children } : { children: React.ReactNode }) {
+// 部分プレレンダリングの有効化
+// next.config.tsの設定から使えるようにしている
+export const experimental_ppr = true;
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
